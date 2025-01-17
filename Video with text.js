dd09ee918f620d011687481ab2b2e44cabccb52f
amr099 <amr70210@gmail.com>
2025-01-17 15:46:05 +0200

    custom video

---
+++ Video with text.js
@@ -0,0 +1,40 @@
+/* JS */
+
+const section = document.createElement("section");
+section.classList.add("full-width-video-section");
+
+const video = document.createElement("video");
+video.src =
+    "https://res.cloudinary.com/dpbnqiazo/video/upload/v1726912315/rfynlfsgheokpu8lxf6e.mp4";
+video.autoplay = true;
+video.muted = true;
+video.loop = true;
+section.appendChild(video);
+
+const overlay = document.createElement("div");
+overlay.classList.add("video-overlay");
+
+const heading = document.createElement("h1");
+heading.textContent = "أضف لمسة من الأناقة والراحة إلى سيارتك";
+heading.classList.add("overlay-heading");
+overlay.appendChild(heading);
+
+const paragraph = document.createElement("h2");
+paragraph.textContent =
+    "Yاكتشف مجموعة إكسسوارات السيارة التي تجمع بين التصميم العملي والأناقة العصرية، لجعل كل رحلة أكثر راحة وتميزاً.";
+paragraph.classList.add("overlay-paragraph");
+overlay.appendChild(paragraph);
+
+const button = document.createElement("button");
+button.textContent = "اشتري الان";
+button.classList.add("btn-changing");
+overlay.appendChild(button);
+
+section.appendChild(overlay);
+
+const elFeatures = document.querySelector(".image-text");
+if (elFeatures) {
+    elFeatures.parentNode.insertBefore(section, elFeatures);
+} else {
+    console.error('Element with class "el_features" not found.');
+}
