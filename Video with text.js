4c35635c91d4b7c2f408bd61ee494c6018c2b648
Amr Ahmad <58093769+amr099@users.noreply.github.com>
2025-01-31 13:49:00 +0200

    Update Video with text.js

--- Video with text.js
+++ Video with text.js
@@ -1,5 +1,3 @@
-/* JS */
-
 const section = document.createElement("section");
 section.classList.add("full-width-video-section");
 
@@ -19,22 +17,22 @@ heading.textContent = "أضف لمسة من الأناقة والراحة إلى
 heading.classList.add("overlay-heading");
 overlay.appendChild(heading);
 
-const paragraph = document.createElement("h2");
+const paragraph = document.createElement("p");
 paragraph.textContent =
     "Yاكتشف مجموعة إكسسوارات السيارة التي تجمع بين التصميم العملي والأناقة العصرية، لجعل كل رحلة أكثر راحة وتميزاً.";
 paragraph.classList.add("overlay-paragraph");
 overlay.appendChild(paragraph);
 
 const button = document.createElement("button");
-button.textContent = "اشتري الان";
+button.textContent = "تسوق الان";
 button.classList.add("btn-changing");
 overlay.appendChild(button);
 
 section.appendChild(overlay);
 
-const elFeatures = document.querySelector(".image-text");
-if (elFeatures) {
-    elFeatures.parentNode.insertBefore(section, elFeatures);
+const prevSection = document.querySelector(".g_cat");
+if (prevSection) {
+    prevSection.parentNode.insertBefore(section, prevSection);
 } else {
-    console.error('Element with class "el_features" not found.');
+    console.error('previous section not found.');
 }
